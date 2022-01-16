7cc0d59ffec4135ce5d3712f61ae555d017cee4f
Ari Nugraha <nugrahaa878@gmail.com>
2022-01-16 09:31:56 +0700

    component(navbar): create navbar component

--- src/containers/Dashboard/index.jsx
+++ src/containers/Dashboard/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../../components/Navbar';
 
 const Dashboard = () => {
   return (
     <div>
+      <Navbar />
       <p>Dashboard</p>
     </div>
   );
