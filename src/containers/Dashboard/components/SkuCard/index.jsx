1d35bba2319c848ac6c46b76f737ca2dcbbdbb8b
Ari Nugraha <nugrahaa878@gmail.com>
2022-01-17 08:19:11 +0700

    component(card): create top product card

--- src/containers/Dashboard/components/SkuCard/index.jsx
+++ src/containers/Dashboard/components/SkuCard/index.jsx
@@ -4,24 +4,40 @@ import styles from './styles.module.css';
 
 import more from '../../../../assets/icons/more.png';
 import ProductCard from '../ProductCard';
+import TopProductCard from '../TopProductCard';
 
 const SkuCard = ({
   title,
   products,
 }) => {
-  const listProduct = products.map((item) => (
-    <ProductCard 
-      image={item.image}
-      name={item.name}
-      price={item.price}
-      sold={item.sold}
-    />
-  ));
+  const listProduct = products.map((item, index) => {
+    return (
+      (index === 0)
+        ? (
+          <TopProductCard
+            image={item.image}
+            name={item.name}
+            price={item.price}
+            sold={item.sold}
+            key={index}
+          />
+        )
+        : (
+          <ProductCard
+            image={item.image}
+            name={item.name}
+            price={item.price}
+            sold={item.sold}
+            key={index}
+          />
+        )
+    );
+  });
 
   return (
     <div className={styles.container}>
       <h3 className={styles.title}>{title}</h3>
-      <img className={styles.more} src={more} alt=''/>
+      <img className={styles.more} src={more} alt='' />
       {listProduct}
     </div>
   );
---
+++ src/containers/Dashboard/components/TopProductCard/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import styles from './styles.module.css';
+
+const TopProductCard = ({
+  image,
+  name,
+  price,
+  sold,
+}) => {
+  return (
+    <div className={styles.container}>
+      <img className={styles.image} src={image} alt=''/>
+      <div className={styles.info}>
+        <h3>{name}</h3>
+        <div className={styles.sales}>
+          <p>Rp {price}</p>
+          <p>{sold}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopProductCard;
