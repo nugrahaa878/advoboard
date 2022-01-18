fa3c215b15c0921b63ec162bcecb88955b7dad4a
Ari Nugraha <nugrahaa878@gmail.com>
2022-01-18 14:13:15 +0700

    chore: change wording navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -12,8 +12,8 @@ const Navbar = () => {
       <img className={styles.mainLogo} src={mainLogo} alt='' />
       <ul className={styles.listMenu}>
         <li className={styles.userInfo}>
-          <p className={styles.username}>Username</p>
-          <p className={styles.companyName}>Company Name</p>
+          <p className={styles.username}>Ari</p>
+          <p className={styles.companyName}>Advotics</p>
         </li>
         <li><img className={styles.profile} src={profile} alt='' /></li>
         <li><img className={styles.logout} src={logout} alt='' /></li>
