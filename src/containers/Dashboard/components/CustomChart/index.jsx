aaaa000919bd7e2c1d2633b5e46d542bc227d87c
Ari Nugraha <nugrahaa878@gmail.com>
2022-01-18 12:39:21 +0700

    data: add dummy data for chart

--- src/containers/Dashboard/components/CustomChart/index.jsx
+++ src/containers/Dashboard/components/CustomChart/index.jsx
@@ -14,57 +14,7 @@ import styles from './styles.module.css';
 
 import arrow from '../../../../assets/icons/arrow-down.png';
 import more from '../../../../assets/icons/more.png';
-
-const data = [
-  {
-    name: "Page A",
-    uv: 590,
-    pv: 800,
-    amt: 1400,
-    cnt: 490,
-    upt: 8,
-  },
-  {
-    name: "Page B",
-    uv: 868,
-    pv: 967,
-    amt: 1506,
-    cnt: 590,
-    upt: 9,
-  },
-  {
-    name: "Page C",
-    uv: 1397,
-    pv: 1098,
-    amt: 989,
-    cnt: 350,
-    upt: 10,
-  },
-  {
-    name: "Page D",
-    uv: 1480,
-    pv: 1200,
-    amt: 1228,
-    cnt: 480,
-    upt: 5,
-  },
-  {
-    name: "Page E",
-    uv: 1520,
-    pv: 1108,
-    amt: 1100,
-    cnt: 460,
-    upt: 8,
-  },
-  {
-    name: "Page F",
-    uv: 1400,
-    pv: 680,
-    amt: 1700,
-    cnt: 380,
-    upt: 20,
-  }
-];
+import { chartData } from './data';
 
 const CustomChart = () => {
   return (
@@ -82,8 +32,8 @@ const CustomChart = () => {
 
       <ComposedChart
         width={500}
-        height={400}
-        data={data}
+        height={350}
+        data={chartData}
         margin={{
           top: 20,
           right: 20,
@@ -92,14 +42,14 @@ const CustomChart = () => {
         }}
       >
         <CartesianGrid stroke="#f5f5f5" />
-        <XAxis dataKey="name" scale="band" />
+        <XAxis dataKey="day" scale="band" />
         <YAxis />
         <Tooltip />
         <Legend />
-        <Bar dataKey="upt" stackId='a' barSize={20} fill="#707070" />
-        <Bar dataKey="uv" stackId='a' barSize={20} fill="#37B04C" />
-        <Bar dataKey="cnt" stackId='a' barSize={20} fill="#289E45" />
-        <Line type="monotone" dataKey="uv" stroke="#FFE854" />
+        <Bar dataKey="apv" stackId='a' barSize={20} fill="#37B04C" />
+        <Bar dataKey="nett" stackId='a' barSize={20} fill="#289E45" />
+        <Bar dataKey="upt" stackId='a' barSize={20} fill="#79d891" />
+        <Line type="monotone" dataKey="gross" stroke="#FFE854" />
       </ComposedChart>
     </div>
 
