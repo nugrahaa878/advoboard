c43bfc394ddc9e79be178d3e9145d3ac179f95aa
Ari Nugraha <nugrahaa878@gmail.com>
2022-01-18 13:42:35 +0700

    style: fix styling navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,27 +2,20 @@ import React from 'react';
 
 import styles from './styles.module.css';
 
-import hamburger from '../../assets/icons/hamburger.png';
-import close from '../../assets/icons/close.png';
 import profile from '../../assets/icons/profile.png';
 import logout from '../../assets/icons/logout.png';
-import mainLogo from '../../assets/images/advotics-logo.jpg';
+import mainLogo from '../../assets/images/advotics-logo-2.jpg';
 
 const Navbar = () => {
   return (
     <nav>
-      <input type='checkbox' id={styles.check} />
-      <label for={styles.check}>
-        <img className={styles.hamburger} src={hamburger} id={styles.btn} alt='' />
-        <img className={styles.close} src={close} id={styles.close} alt='' />
-      </label>
       <img className={styles.mainLogo} src={mainLogo} alt='' />
       <ul className={styles.listMenu}>
         <li className={styles.userInfo}>
           <p className={styles.username}>Username</p>
           <p className={styles.companyName}>Company Name</p>
         </li>
-        <li><img className={styles.profile} src={profile} alt=''/></li>
+        <li><img className={styles.profile} src={profile} alt='' /></li>
         <li><img className={styles.logout} src={logout} alt='' /></li>
       </ul>
     </nav>
